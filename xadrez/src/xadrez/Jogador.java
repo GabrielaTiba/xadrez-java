2bec11af9b806e1862d86ddd08a04621a3ab6f5a
clara-de-ana <150181346+clara-de-ana@users.noreply.github.com>
2024-09-06 19:26:22 -0300

    Update Jogador.java

--- xadrez/src/xadrez/Jogador.java
+++ xadrez/src/xadrez/Jogador.java
@@ -16,15 +16,42 @@ public class Jogador {
         this.cor = cor;
         this.pecasAtivas = new ArrayList<>();
         this.pecasCapturadas = new ArrayList<>();
+        inicializarPecas();
     }
-    
-    public void receberPecas(Peca p) {
-        pecasAtivas.add(p);
-    }
-    
-    public boolean ehDoJogador(Peca p) {
-        if(pecasAtivas.contains(p)) return true;
-        return false;
+
+    // preenche a lista de peças ativas com as peças iniciais do jogador
+    // torre posicionada nas extremidades do tabuleiro
+    // cavalo ao lado da torre
+    // bispo ao lado do cavalo
+    // rainha na casa central que corresponde à sua cor, ao lado do bispo
+    // rei ao lado da rainha
+    // oito peões posicionados na frente de todas essas peças
+    private void inicializarPecas() {
+        if (cor.equals("branco")) {
+            pecasAtivas.add(new Torre("branco", false));
+            pecasAtivas.add(new Cavalo("branco", false));
+            pecasAtivas.add(new Bispo("branco", false));
+            pecasAtivas.add(new Dama("branco", false));
+            pecasAtivas.add(new Rei("branco", false));
+            pecasAtivas.add(new Bispo("branco", false));
+            pecasAtivas.add(new Cavalo("branco", false));
+            pecasAtivas.add(new Torre("branco", false));
+            for (int i = 0; i < 8; i++) {
+                pecasAtivas.add(new Peao("branco", false));
+            }
+        } else {
+            pecasAtivas.add(new Torre("preto", false));
+            pecasAtivas.add(new Cavalo("preto", false));
+            pecasAtivas.add(new Bispo("preto", false));
+            pecasAtivas.add(new Dama("preto", false));
+            pecasAtivas.add(new Rei("preto", false));
+            pecasAtivas.add(new Bispo("preto", false));
+            pecasAtivas.add(new Cavalo("preto", false));
+            pecasAtivas.add(new Torre("preto", false));
+            for (int i = 0; i < 8; i++) {
+                pecasAtivas.add(new Peao("preto", false));
+            }
+        }
     }
 
     // solicita a jogada do jogador
@@ -34,28 +61,40 @@ public class Jogador {
         return scanner.nextLine();
     }
 
+    // retorna uma string com as peças capturadas
+    public List<Peca> pecasCapturadas() {
+        return pecasCapturadas;
+    }
+
     // remove a peça especificada da lista de peças ativas e adiciona à lista de peças capturadas
     public void capturarPeca(Peca peca) {
         if (pecasAtivas.remove(peca)) {
             pecasCapturadas.add(peca);
-            peca.capturar();
         }
     }
 
+    // Getters e Setters
     public String getNome() {
         return nome;
     }
-    
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
     public String getCor() {
         return cor;
     }
-    
-    public void mostrarCapturadas() {
-        if (!pecasCapturadas.isEmpty()) {
-            for (Peca peca : pecasCapturadas) {
-                System.out.print(peca.desenha() + " "); 
-            }
-            System.out.println("");
-        }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public List<Peca> getPecasAtivas() {
+        return pecasAtivas;
+    }
+
+    public List<Peca> getPecasCapturadas() {
+        return pecasCapturadas;
     }
 }
