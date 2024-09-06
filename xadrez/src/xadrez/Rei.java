98f87f9e4773294031efaac35454ff8115d6b525
clara-de-ana <150181346+clara-de-ana@users.noreply.github.com>
2024-09-06 19:29:06 -0300

    Update Rei.java

--- xadrez/src/xadrez/Rei.java
+++ xadrez/src/xadrez/Rei.java
@@ -2,13 +2,13 @@ package xadrez;
 
 public class Rei extends Peca {
     
-    public Rei(String cor) {
-        super(cor);
+    public Rei(String cor, boolean estado) {
+        super(cor, estado);
     }
     
     @Override 
     public boolean movimentoValido(int linhaO, char colunaO, int linhaD, char colunaD) {
-        if (estado) { //se a peça ainda está no jogo
+        if (capturada) { //se a peça ainda está no jogo
             
             int dLinha = Math.abs(linhaO - linhaD);
             int dColuna = Math.abs(colunaO - colunaD); //aparentemente, Math.abs deixa as contas em valor absoluto
@@ -31,18 +31,7 @@ public class Rei extends Peca {
     }
     
     @Override 
-    public String desenha() {
-        if (cor.equals("Branco")) return "R"; //se a cor for branca, retorna maiúsculo
-        return "r"; //se não, minúsculo
-    }
-    
-    /*@Override 
     public String desenho() {
         return this.cor.equals("branco") ? "♔" : "♚";
-    }*/
-    
-    @Override
-    public String tipo() {
-        return "Rei";
     }
 }
