50191eda82a12b6c3bf246333052cb30ff849a41
clara-de-ana <150181346+clara-de-ana@users.noreply.github.com>
2024-09-06 19:23:27 -0300

    Update Casa.java

--- xadrez/src/xadrez/Casa.java
+++ xadrez/src/xadrez/Casa.java
@@ -15,18 +15,10 @@ public class Casa {
         pecaNaCasa = null; //não há peças nela
         
         if(linha%2 == 0 && coluna%2 == 0 || linha%2 != 0 && coluna%2 != 0) {
-            cor = "Preto"; //casas com linhas e colunas ímpares ou linhas e colunas pares são pretas
+            cor = "preto"; //casas com linhas e colunas ímpares ou linhas e colunas pares são pretas
         } else {
-            cor = "Branco"; //par com ímpar é branco
+            cor = "branco"; //par com ímpar é branco
         }
-    }
-    
-    public int getLinha() {
-        return linha;
-    }
-    
-    public char getColuna() {
-        return coluna;
     }
     
     public void ocupar(Peca peca) {
@@ -39,24 +31,36 @@ public class Casa {
         pecaNaCasa = null;
     } //se a peça for sair
     
-    public boolean estaOcupada() {
-        return ocupada;
-    }
-    
-    public Peca getPeca() {
-        if(ocupada) {
-            return pecaNaCasa;
-        }
-        
-        return null;
-    }
-    
     public String desenha() {
         if(ocupada) {
-           return pecaNaCasa.desenha() + " ";
+           return pecaNaCasa.desenho() + " ";
         } 
         
         return "[]";
     }
-       
+
+    public boolean isOcupada() {
+        return ocupada;
+    }
+
+    public void setOcupada(boolean ocupada) {
+        this.ocupada = ocupada;
+    }
+
+    public Peca getPecaNaCasa() {
+        return pecaNaCasa;
+    }
+
+    public void setPecaNaCasa(Peca pecaNaCasa) {
+        this.pecaNaCasa = pecaNaCasa;
+    }
+    
+    public int getLinha(){
+        return this.linha;
+    }
+    
+    public char getColuna(){
+        return this.coluna;
+    }
+    
 }
